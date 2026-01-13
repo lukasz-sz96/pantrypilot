08d150d542606209f4d405c4747fd4ade90e3990
Łukasz <lukasz.szczesny96@gmail.com>
2026-01-13 23:39:30 +0100

    cleanup boilerplate

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,31 +21,10 @@ export const Route = createRootRouteWithContext<{
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'PantryPilot',
       },
     ],
-    links: [
-      { rel: 'stylesheet', href: appCss },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
-      },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' },
-    ],
+    links: [{ rel: 'stylesheet', href: appCss }],
   }),
   notFoundComponent: () => <div>Route not found</div>,
   component: RootComponent,
